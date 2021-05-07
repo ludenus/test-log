350bf8aafcf5fd2104db0857ee80c09208295ccc
Andriy Andryeyev <a.andryeyev@spd-ukraine.com>
2021-05-07 20:42:35 +0300

    add log4j log test

---
+++ test-log4j/app/src/test/kotlin/test/log4j/LogTest.kt
@@ -0,0 +1,38 @@
+package test.log4j
+
+import org.apache.logging.log4j.LogManager.getLogger
+import java.util.*
+import kotlin.system.measureTimeMillis
+import kotlin.test.Test
+import kotlin.test.assertFalse
+
+class LogTest {
+    val log = getLogger(javaClass)
+
+    val ITERARTIONS: Int = 100_000_000
+
+    @Test
+    fun logString() {
+        assertFalse { log.isTraceEnabled }
+
+        val time = measureTimeMillis {
+            repeat(ITERARTIONS) {
+                log.trace("it=$it random=${Random()}")
+            }
+        }
+        log.info("logString iterations: {} time: {} ms", ITERARTIONS, time)
+    }
+
+    @Test
+    fun logTemplate() {
+        assertFalse { log.isTraceEnabled }
+
+        val time = measureTimeMillis {
+            repeat(ITERARTIONS) {
+                log.trace("it={} random={}", it, Random())
+            }
+        }
+        log.info("logTemplate iterations: {} time: {} ms", ITERARTIONS, time)
+    }
+
+}
