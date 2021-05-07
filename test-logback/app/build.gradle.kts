268bf893fae2d014aafdfcbd8099f7f96e3afe4b
Andriy Andryeyev <a.andryeyev@spd-ukraine.com>
2021-05-07 20:13:26 +0300

    add LogTest

--- test-logback/app/build.gradle.kts
+++ test-logback/app/build.gradle.kts
@@ -34,6 +34,11 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-core:1.2.3")
+
 }
 
 application {
