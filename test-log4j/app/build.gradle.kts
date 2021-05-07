111142b82ba96832b77ffc1329e271b673ade224
Andriy Andryeyev <a.andryeyev@spd-ukraine.com>
2021-05-07 21:58:51 +0300

    add logLambda

--- test-log4j/app/build.gradle.kts
+++ test-log4j/app/build.gradle.kts
@@ -35,8 +35,7 @@ dependencies {
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
-//    implementation("org.apache.logging.log4j:log4j-api-kotlin:1.0.0")
-//    implementation("org.apache.logging.log4j:log4j:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-api-kotlin:1.0.0")
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
 }
--- test-log4j/app/src/test/kotlin/test/log4j/LogTest.kt
+++ test-log4j/app/src/test/kotlin/test/log4j/LogTest.kt
@@ -1,23 +1,27 @@
 package test.log4j
 
 import org.apache.logging.log4j.LogManager.getLogger
+import org.apache.logging.log4j.kotlin.logger
 import java.util.*
 import kotlin.system.measureTimeMillis
 import kotlin.test.Test
 import kotlin.test.assertFalse
 
 class LogTest {
-    val log = getLogger(javaClass)
 
     val ITERARTIONS: Int = 100_000_000
 
     @Test
     fun logString() {
+        val log = getLogger("logString")
         assertFalse { log.isTraceEnabled }
 
         val time = measureTimeMillis {
             repeat(ITERARTIONS) {
                 log.trace("it=$it random=${Random()}")
+                if (it == ITERARTIONS - 1) {
+                    log.error("it=$it random=${Random()}")
+                }
             }
         }
         log.error("logString iterations: {} time: {} ms", ITERARTIONS, time)
@@ -25,14 +29,32 @@ class LogTest {
 
     @Test
     fun logTemplate() {
+        val log = getLogger("logTemplate")
+
         assertFalse { log.isTraceEnabled }
 
         val time = measureTimeMillis {
             repeat(ITERARTIONS) {
                 log.trace("it={} random={}", it, Random())
+                if (it == ITERARTIONS - 1) {
+                    log.error("it={} random={}", it, Random())
+                }
             }
         }
         log.error("logTemplate iterations: {} time: {} ms", ITERARTIONS, time)
     }
 
+    @Test
+    fun logLambda() {
+        val log = logger("logLambda")
+        val time = measureTimeMillis {
+            repeat(ITERARTIONS) {
+                log.trace { "it=${it} random=${Random()}" }
+                if (it == ITERARTIONS - 1) {
+                    log.error { "it=${it} random=${Random()}" }
+                }
+            }
+        }
+        log.error { "logLambda iterations: ${ITERARTIONS} time: ${time} ms" }
+    }
 }
